3be92288df9b26ead45c1eaa34995d78e43ddaf0
Richard Mamaril Rodriguez <richardmamarilrodriguez@gmail.com>
2024-02-12 02:15:30 -0600

    cargo lib init

---
+++ target/debug/deps/fountain_parser_rs-1c4e42c01a227ac8.d
@@ -0,0 +1,5 @@
+/home/rich/Projects/Software Development/fountain-parser-rs/target/debug/deps/libfountain_parser_rs-1c4e42c01a227ac8.rmeta: src/lib.rs
+
+/home/rich/Projects/Software Development/fountain-parser-rs/target/debug/deps/fountain_parser_rs-1c4e42c01a227ac8.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/fountain_parser_rs-f11f022fa3ca6b78.d
@@ -0,0 +1,5 @@
+/home/rich/Projects/Software Development/fountain-parser-rs/target/debug/deps/libfountain_parser_rs-f11f022fa3ca6b78.rmeta: src/lib.rs
+
+/home/rich/Projects/Software Development/fountain-parser-rs/target/debug/deps/fountain_parser_rs-f11f022fa3ca6b78.d: src/lib.rs
+
+src/lib.rs:
